873237982327a5486bb47df846f952d86b99ef21
ishaquehassan <ishaquehassan@gmail.com>
2017-03-02 02:03:10 +0500

    Just Fixed Crashes

---
+++ app/src/main/java/edu/educationportal/Splashscreen.java
@@ -0,0 +1,48 @@
+package edu.educationportal;
+
+import android.os.Bundle;
+
+
+/**
+ * Created by erumk on 01-Dec-16.
+ */
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+
+
+public class Splashscreen extends Activity {
+
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(Splashscreen.this, MainActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+}
+
+
